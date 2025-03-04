e293b362988f8cdd65bb3f7e256d55bad721f643
Jeff Mitchell <jeffreytm@gmail.com>
2025-03-03 20:36:32 -0500

    Update Student.java

--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,26 +1,66 @@
 package sba.sms.models;
 
+
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
-
-
-/**
- * Student is a POJO, configured as a persistent class that represents (or maps to) a table
- * name 'student' in the database. A Student object contains fields that represent student
- * login credentials and a join table containing a registered student's email and course(s)
- * data. The Student class can be viewed as the owner of the bi-directional relationship.
- * Implement Lombok annotations to eliminate boilerplate code.
- */
-
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "student")
+@Entity
 public class Student {
-
-
+    @NonNull
+    @Id @Column(length = 50,name = "email")
+    String email;
+    @NonNull
+    @Column(length = 50, nullable = false, name = "name")
+    String name;
+    @NonNull
+    @Column(length = 50,name = "password")
+    String password;
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    @JoinTable(name = "student_courses",
+            joinColumns = @JoinColumn(name = "student_email"),
+            inverseJoinColumns = @JoinColumn(name = "courses_id"))
+    Set<Course> courses = new LinkedHashSet<>();
+    public void addCourse(Course c){
+        this.courses.add(c);
+        c.getStudents().add(this);
     }
+  
+    public Student(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return email.equals(student.email) && name.equals(student.name) && password.equals(student.password);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, name, password);
+    }
+    
+    public String getName() {
+        return this.name; 
+    }
+	public Object getPassword() {
+		return this.getPassword();
+	}
+}
 
 
 
